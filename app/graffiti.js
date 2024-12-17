6698184a7e3cba019e8503f7d26acda20fd819e7
jahuestis <160439894+jahuestis@users.noreply.github.com>
2024-12-17 00:22:33 -0500

    Implemented canvas zoom
    Canvas repositioning not yet implemented, so it just zooms on the top left corner.

--- app/graffiti.js
+++ app/graffiti.js
@@ -2,25 +2,28 @@ var canvas;
 var context;
 var tiles;
 
+var pixels;
+var width;
+var height;
 var canvasLoaded = false;
+const upscale = 10;
+var canvasPosition = [0, 0];
 
 const colors = ['black', 'red', 'orange', 'yellow', 'lime', 'cyan', 'magenta', 'white'];
 var mouseX = 0;
 var mouseY = 0;
+var zoom = 1;
 
 const colorButtons = Array.from(document.getElementsByName("color"));
 var colorSelected = Math.floor(Math.random() * colors.length);
 colorButtons.forEach(color => {
-    console.log(color.value);
     if (color.value == colorSelected) {
         color.checked = true;
     }
     color.addEventListener('change', function() {
         colorSelected = this.value;
-        console.log(colorSelected);
     });
 });
-console.log(colorSelected);
 
 
 // -- Server/Client handling -- 
@@ -32,13 +35,11 @@ socket.addEventListener('message', (event) => {
 
     if (messageType === 'fullcanvas') { 
         data = JSON.parse(message.data);
-        canvas.width = data.width;
-        canvas.height = data.height;
-        for (let x = 0; x < data.width; x++) {
-            for (let y = 0; y < data.height; y++) {
-                drawPixel(x, y, data.tiles[x][y]);
-            }
-        }
+        pixels = data.tiles;
+        width = data.width;
+        height = data.height;
+        canvas.width = width * upscale;
+        canvas.height = height * upscale;
         canvasLoaded = true;
     } else if (messageType === 'pixelchange') {
         const data = JSON.parse(message.data);
@@ -46,7 +47,7 @@ socket.addEventListener('message', (event) => {
         const pixelColor = data.color;
 
         console.log(`pixelchange: ${pixelPosition[0]}, ${pixelPosition[1]}, ${pixelColor}`);
-
+        pixels[pixelPosition[0]][pixelPosition[1]] = pixelColor;
         drawPixel(pixelPosition[0], pixelPosition[1], pixelColor);
     } else {
         console.log(event.data);
@@ -75,6 +76,25 @@ window.onload = () => {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
 
+    canvas.addEventListener('wheel', (event) => {
+        event.preventDefault();
+
+        if (event.deltaY < 0) {
+            zoom = Math.min(zoom * 1.2, 10); // Increase zoom by 10% (max zoom is 5x)
+        } else {
+            zoom = Math.max(zoom * 0.8, 1); // Decrease zoom by 10% (min zoom is 0.1x)
+        }
+
+        console.log(`Zoom: ${zoom}`);
+
+    }, { passive: false });
+
+    canvas.addEventListener('click', function(event) {
+        if (canvasLoaded && mouseX >= 0 && mouseX < canvas.width && mouseY >= 0 && mouseY < canvas.height) {
+            socket.send(jsonMessage('pixelchange', jsonPixel(mouseX, mouseY, parseInt(colorSelected))));
+        }
+    });
+
     requestAnimationFrame(update);
 }
 
@@ -82,12 +102,18 @@ document.addEventListener('mousemove', function(event) {
     bounds = canvas.getBoundingClientRect();
     scaleX = canvas.width / bounds.width;
     scaleY = canvas.height / bounds.height;
-    mouseX = Math.floor((event.clientX - bounds.left) * scaleX);
-    mouseY = Math.floor((event.clientY - bounds.top) * scaleY);
+    mouseX = Math.floor((event.clientX - bounds.left) * scaleX / zoom / upscale);
+    mouseY = Math.floor((event.clientY - bounds.top) * scaleY / zoom / upscale);
 });
 
 function update() {
-    //console.log(`mouse: ${mouseX}, ${mouseY}`);
+    if (canvasLoaded) {
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                drawPixel(x, y, pixels[x][y]);
+            }
+        }
+    }
 
     // update on next frame
     requestAnimationFrame(update);
@@ -95,12 +121,8 @@ function update() {
 
 function drawPixel(x, y, color) {
     context.fillStyle = colors[color];
-    context.fillRect(x, y, 1, 1);
+    context.fillRect(x * zoom * upscale, y * zoom * upscale, zoom * upscale, zoom * upscale);
 }
 
-document.addEventListener('click', function(event) {
-    if (canvasLoaded && mouseX >= 0 && mouseX < canvas.width && mouseY >= 0 && mouseY < canvas.height) {
-        console.log(jsonPixel(mouseX, mouseY, parseInt(colorSelected)));
-        socket.send(jsonMessage('pixelchange', jsonPixel(mouseX, mouseY, parseInt(colorSelected))));
-    }
-});
+
+

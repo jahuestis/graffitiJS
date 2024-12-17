35a9ae082d240b77f8b2ce70d0ef19e346c247c5
jahuestis <160439894+jahuestis@users.noreply.github.com>
2024-12-16 23:54:28 -0500

    Clients can now update pixel colors
    Cool down not yet implemented

--- app/graffiti.js
+++ app/graffiti.js
@@ -8,13 +8,29 @@ const colors = ['black', 'red', 'orange', 'yellow', 'lime', 'cyan', 'magenta', '
 var mouseX = 0;
 var mouseY = 0;
 
+const colorButtons = Array.from(document.getElementsByName("color"));
+var colorSelected = Math.floor(Math.random() * colors.length);
+colorButtons.forEach(color => {
+    console.log(color.value);
+    if (color.value == colorSelected) {
+        color.checked = true;
+    }
+    color.addEventListener('change', function() {
+        colorSelected = this.value;
+        console.log(colorSelected);
+    });
+});
+console.log(colorSelected);
+
+
 // -- Server/Client handling -- 
 const socket = new WebSocket('ws://localhost:3000')
 
 socket.addEventListener('message', (event) => {
     const message = JSON.parse(event.data);
+    const messageType = message.type;
 
-    if (message.type === 'fullcanvas') { 
+    if (messageType === 'fullcanvas') { 
         data = JSON.parse(message.data);
         canvas.width = data.width;
         canvas.height = data.height;
@@ -24,18 +40,43 @@ socket.addEventListener('message', (event) => {
             }
         }
         canvasLoaded = true;
-    }
-    if (message.type === 'pixelchange') {
+    } else if (messageType === 'pixelchange') {
         const data = JSON.parse(message.data);
         const pixelPosition = data.position;
         const pixelColor = data.color;
+
+        console.log(`pixelchange: ${pixelPosition[0]}, ${pixelPosition[1]}, ${pixelColor}`);
+
         drawPixel(pixelPosition[0], pixelPosition[1], pixelColor);
+    } else {
+        console.log(event.data);
     }
 });
+
+function jsonMessage(type, data) {
+    return JSON.stringify({
+        type: type,
+        data: data
+    });
+}
+
+function jsonPixel(x, y, color) {
+    return JSON.stringify({
+        position: [x, y],
+        color: color
+    });
+}
 
 
 
 // -- Front End --
+
+window.onload = () => {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+
+    requestAnimationFrame(update);
+}
 
 document.addEventListener('mousemove', function(event) {
     bounds = canvas.getBoundingClientRect();
@@ -44,13 +85,6 @@ document.addEventListener('mousemove', function(event) {
     mouseX = Math.floor((event.clientX - bounds.left) * scaleX);
     mouseY = Math.floor((event.clientY - bounds.top) * scaleY);
 });
-
-window.onload = () => {
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
-
-    requestAnimationFrame(update);
-}
 
 function update() {
     //console.log(`mouse: ${mouseX}, ${mouseY}`);
@@ -63,3 +97,10 @@ function drawPixel(x, y, color) {
     context.fillStyle = colors[color];
     context.fillRect(x, y, 1, 1);
 }
+
+document.addEventListener('click', function(event) {
+    if (canvasLoaded && mouseX >= 0 && mouseX < canvas.width && mouseY >= 0 && mouseY < canvas.height) {
+        console.log(jsonPixel(mouseX, mouseY, parseInt(colorSelected)));
+        socket.send(jsonMessage('pixelchange', jsonPixel(mouseX, mouseY, parseInt(colorSelected))));
+    }
+});
--- server/index.js
+++ server/index.js
@@ -6,14 +6,71 @@ const socket = new WebSocketServer({
 var clients = new Map();
 const width = 128;
 const height = 128;
-var tiles = Array.from({ length: width }, () => Array(height).fill(Math.floor(Math.random() * 8)));
+var tiles = Array.from({ length: width }, () => Array(height).fill(0));
 
 
 socket.on('connection', (ws) => {
+    clients.set(ws, Date.now());
     console.log("Client conncted");
     console.log("Sending client full canvas");
     ws.send(jsonMessage('fullcanvas', jsonCanvas(tiles, width, height)));
+
+    ws.on('message', (message) => {
+        try {
+            const messageStr = message instanceof Buffer ? message.toString() : message;
+            const messageJSON = JSON.parse(messageStr);
+
+            if (messageJSON.type === 'pixelchange') {
+                const data = JSON.parse(messageJSON.data);
+                const pixelPosition = data.position;
+                const pixelColor = data.color;
+                if (pixelPosition[0] < 0 || pixelPosition[0] >= width || pixelPosition[1] < 0 || pixelPosition[1] >= height) {
+                    throw new Error("Invalid pixel position");
+                }
+                if (pixelColor < 0 || pixelColor > 7) {
+                    throw new Error("Invalid pixel color");
+                }
+                tiles[pixelPosition[0]][pixelPosition[1]] = pixelColor;
+                broadcast(jsonMessage('pixelchange', jsonPixel(pixelPosition[0], pixelPosition[1], pixelColor)));
+            }
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    ws.on('close', () => {
+        clients.delete(ws);
+        console.log('Client disconnected');
+    });
+
+    ws.on('error', (error) => {
+        console.error(`Error on channel ${clients.get(ws)}: ${error.message}`);
+    });
+
 });
+
+function broadcast(message) {
+    var count = 0;
+    clients.forEach((value, client) => {
+        try {
+            client.send(message);
+            count++;
+        } catch (error) {
+            console.log(error);
+        }
+    })
+    console.log(`Broadcasted to ${count} clients`);
+}
+
+
+function jsonMessage(type, data) {
+    return JSON.stringify({
+        type: type,
+        data: data
+    });
+}
 
 function jsonCanvas(tiles, width, height) {
     return JSON.stringify({
@@ -22,9 +79,10 @@ function jsonCanvas(tiles, width, height) {
         height: height
     })
 }
-function jsonMessage(type, data) {
+
+function jsonPixel(x, y, color) {
     return JSON.stringify({
-        type: type,
-        data: data
+        position: [x, y],
+        color: color
     });
 }

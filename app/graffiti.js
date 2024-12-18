03e42b9a99604c87c406511ddccaf7cceac461aa
jahuestis <160439894+jahuestis@users.noreply.github.com>
2024-12-17 21:52:39 -0500

    Zoom and offset implemented
    Scroll to zoom in/out, right click and drag to change offset.
    Zoom still needs work, currently zooms relative to center of art when it should zoom relative to actual center of screen

--- app/graffiti.js
+++ app/graffiti.js
@@ -7,12 +7,17 @@ var width;
 var height;
 var canvasLoaded = false;
 const upscale = 8;
-var canvasPosition;
+var canvasOffset = [0, 0];
 
 const colors = ['black', 'red', 'orange', 'yellow', 'lime', 'cyan', 'magenta', 'white'];
+var mouseXPrev = 0;
+var mouseYPrev = 0;
 var mouseX = 0;
 var mouseY = 0;
+var gridX = 0;
+var gridY = 0;
 var zoom = 1;
+var offsetting = false;
 
 const colorButtons = Array.from(document.getElementsByName("color"));
 var colorSelected = Math.floor(Math.random() * colors.length);
@@ -40,7 +45,6 @@ socket.addEventListener('message', (event) => {
         height = data.height;
         canvas.width = width * upscale;
         canvas.height = height * upscale;
-        canvasPosition = [width / 2, height / 2];
         canvasLoaded = true;
     } else if (messageType === 'pixelchange') {
         const data = JSON.parse(message.data);
@@ -49,7 +53,6 @@ socket.addEventListener('message', (event) => {
 
         console.log(`pixelchange: ${pixelPosition[0]}, ${pixelPosition[1]}, ${pixelColor}`);
         pixels[pixelPosition[0]][pixelPosition[1]] = pixelColor;
-        drawPixel(pixelPosition[0], pixelPosition[1], pixelColor);
     } else {
         console.log(event.data);
     }
@@ -76,14 +79,13 @@ function jsonPixel(x, y, color) {
 window.onload = () => {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
-
     canvas.addEventListener('wheel', (event) => {
         event.preventDefault();
 
         if (event.deltaY < 0) {
-            zoom = Math.min(zoom * 1.2, 10); // Increase zoom by 10% (max zoom is 5x)
+            zoom = Math.min(zoom * 1.25, 10); // Increase zoom by 10% (max zoom is 5x)
         } else {
-            zoom = Math.max(zoom * 0.8, 1); // Decrease zoom by 10% (min zoom is 0.1x)
+            zoom = Math.max(zoom * 0.75, 1); // Decrease zoom by 10% (min zoom is 0.1x)
         }
 
         console.log(`Zoom: ${zoom}`);
@@ -92,22 +94,47 @@ window.onload = () => {
 
     canvas.addEventListener('click', function(event) {
         if (canvasLoaded && mouseX >= 0 && mouseX < canvas.width && mouseY >= 0 && mouseY < canvas.height) {
-            socket.send(jsonMessage('pixelchange', jsonPixel(mouseX, mouseY, parseInt(colorSelected))));
+            socket.send(jsonMessage('pixelchange', jsonPixel(gridX, gridY, parseInt(colorSelected))));
         }
+    });
+
+    canvas.addEventListener('contextmenu', (event) => {
+        event.preventDefault(); // Disable the default right-click menu
     });
 
     requestAnimationFrame(update);
 }
 
+document.addEventListener('mousedown', function(event) {
+    if (event.button === 2) {
+        offsetting = true;
+    }
+})
+
+document.addEventListener('mouseup', function(event) {
+    if (event.button === 2) {
+        offsetting = false;
+    }
+})
+
 document.addEventListener('mousemove', function(event) {
     bounds = canvas.getBoundingClientRect();
     scaleX = canvas.width / bounds.width;
     scaleY = canvas.height / bounds.height;
-    mouseX = Math.floor((event.clientX - bounds.left) * scaleX / zoom / upscale);
-    mouseY = Math.floor((event.clientY - bounds.top) * scaleY / zoom / upscale);
+    mouseX = (event.clientX - bounds.left) * scaleX;
+    mouseY = (event.clientY - bounds.top) * scaleY;
+    gridX = Math.floor((mouseX - (canvasOffset[0] + (canvas.width / 2) - (canvas.width / 2) * zoom)) / zoom / upscale)
+    gridY = Math.floor((mouseY - (canvasOffset[1] + (canvas.height / 2) - (canvas.height / 2) * zoom)) / zoom / upscale)
+    //console.log(`Mouse: ${mouseX}, ${mouseY}, ${gridX}, ${gridY}`);
 });
 
 function update() {
+    if (offsetting) {
+        canvasOffset = [canvasOffset[0] + (mouseX - mouseXPrev), canvasOffset[1] + (mouseY - mouseYPrev)];
+    }
+
+    // draw canvas
+    drawBackground();
     if (canvasLoaded) {
         for (let x = 0; x < width; x++) {
             for (let y = 0; y < height; y++) {
@@ -118,11 +145,23 @@ function update() {
 
     // update on next frame
     requestAnimationFrame(update);
+    mouseXPrev = mouseX;
+    mouseYPrev = mouseY;
+}
+
+function drawBackground() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawPixel(x, y, color) {
     context.fillStyle = colors[color];
-    context.fillRect(x * zoom * upscale, y * zoom * upscale, zoom * upscale, zoom * upscale);
+    context.fillRect(
+        (x) * zoom * upscale + (canvasOffset[0] + (canvas.width / 2) - (canvas.width / 2) * zoom), 
+        (y) * zoom * upscale + (canvasOffset[1] + (canvas.width / 2) - (canvas.height / 2) * zoom), 
+        zoom * upscale, 
+        zoom * upscale
+    );
 }
 
 
